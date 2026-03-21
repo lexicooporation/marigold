1bf1eb46d7f1b12ab30b105f82adb72771cf0ef4
lexi <nwankwoifeanyi2004@gmail.com>
2026-03-21 03:23:24 +0100

    Fix DATABASE_URL config

--- build.sh
+++ build.sh
@@ -5,8 +5,3 @@ pip install -r requirements.txt
 cd marigoldsite
 python manage.py collectstatic --no-input
 python manage.py migrate
-```
-
-**Third, fix your Render start command** to:
-```
-cd marigoldsite && gunicorn marigoldsite.wsgi:application
--- marigoldsite/marigoldsite/settings.py
+++ marigoldsite/marigoldsite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['marigold-id1k.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['marigold-8xdf.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
