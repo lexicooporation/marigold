b6d881b2ffcf080f50364f1d4660ee02ce7c7a30
lexi <nwankwoifeanyi2004@gmail.com>
2026-03-21 06:49:14 +0100

    Fix cloudinary_storage

--- marigoldsite/marigoldsite/settings.py
+++ marigoldsite/marigoldsite/settings.py
@@ -141,10 +141,16 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 #── Cloudinary ────────────────────────────────────────────────
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+cloudinary.config(
+    cloudinary_url=os.getenv('CLOUDINARY_URL')
+)
+
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY':    os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    'CLOUDINARY_URL': os.getenv('CLOUDINARY_URL')
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
