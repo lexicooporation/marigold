3b91ef9c39fb910f87464538b1e399ee3b7b366c
lexi <nwankwoifeanyi2004@gmail.com>
2026-03-21 07:38:44 +0100

    Fix Cloudinary storage config

--- marigoldsite/marigoldsite/settings.py
+++ marigoldsite/marigoldsite/settings.py
@@ -148,9 +148,10 @@ cloudinary.config(
 )
 
 CLOUDINARY_STORAGE = {
-    'CLOUDINARY_URL': os.getenv('CLOUDINARY_URL')
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY':    os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
-
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Email configuration
