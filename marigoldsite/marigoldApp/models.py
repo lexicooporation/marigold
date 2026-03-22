af1c39fbd65f88a42a2fa25082008e393928bc88
lexi <nwankwoifeanyi2004@gmail.com>
2026-03-22 12:24:43 +0100

    Switch Product image to CloudinaryField

--- marigoldsite/marigoldApp/models.py
+++ marigoldsite/marigoldApp/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     ]
 
     name        = models.CharField(max_length=100)  
-    image       = models.ImageField(upload_to='product_images', blank=True, null=True)
+    image       = models.ImageField(blank=True, null=True)
     description = models.TextField()               
     price       = models.DecimalField(decimal_places=2, max_digits=8)
     category    = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='candles')
